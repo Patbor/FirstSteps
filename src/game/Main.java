dea6e871a3738274aed6090042156ae8ae70f154
Patbor <patrykbor97@gmail.com>
2019-10-26 11:53:45 +0200

    Noughts and crosses(without artificial intelligence).

--- src/game/Main.java
+++ src/game/Main.java
@@ -31,7 +31,6 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         String choise = scan.nextLine();
         while (true) {
-
             System.out.println();
             printArray(gra, x, y);
             System.out.println();
@@ -39,45 +38,33 @@ public class Main {
             int X = N.X;
             int Y = N.Y;
 
-            while (gra[X][Y] != ' ') {
+            while (X > 2 || Y > 2 && gra[X][Y] != ' ') {
                 System.out.println("Wpisz nowe współrzedne");
                 N = numbers();
                 X = N.X;
                 Y = N.Y;
             }
-            if ("o".equals(choise)) {
-                gra[X][Y] = 'o';
-            } else {
-                gra[X][Y] = 'x';
-            }
+
+            gra[X][Y] = "o".equals(choise) ? 'o' : 'x';
             ComPoint computer = comNumbers(x, y);
             int a = computer.a;
             int b = computer.b;
-
             while (gra[a][b] != ' ') {
                 computer = comNumbers(x, y);
                 a = computer.a;
                 b = computer.b;
             }
-            if ("o".equals(choise)) {
-
-                gra[a][b] = 'x';
-            } else {
-                gra[a][b] = 'o';
-            }
-
-            if (toWInX(gra, x, y)) {
-                infX(choise);
+            gra[a][b] = "o".equals(choise) ? 'x' : 'o';
+            if (toWIn(gra, x, y, 'x')) {
+                infoAboutWin(choise, "o");
                 if (end()) {
                     clearArray(gra, x, y);
                 } else {
                     break;
                 }
-
-
             }
-            if (toWInO(gra, x, y)) {
-                infO(choise);
+            if (toWIn(gra, x, y, 'x')) {
+                infoAboutWin(choise, "x");
                 if (end()) {
                     clearArray(gra, x, y);
                 } else {
@@ -98,21 +85,10 @@ public class Main {
         }
     }
 
-    private static void infX(String sign) {
-        if ("x".equals(sign)) {
-            System.out.println("Wygrałeś");
-        } else {
-            System.out.println("Przegrałeś");
-        }
+    private static void infoAboutWin(String sign, String noughtsOrCrosses) {
+        System.out.println(noughtsOrCrosses.equals(sign) ? "Wygrałeś" : "Przegrałeś");
     }
 
-    private static void infO(String sign) {
-        if ("o".equals(sign)) {
-            System.out.println("Wygrałeś");
-        } else {
-            System.out.println("Przegrałeś");
-        }
-    }
 
     private static ComPoint comNumbers(int rozX, int rozY) {
         Random rand = new Random();
@@ -159,7 +135,8 @@ public class Main {
         }
     }
 
-    private static boolean toWInX(char[][] tablica, int SizeX, int SizeY) {
+
+    private static boolean toWIn(char[][] tablica, int SizeX, int SizeY, char noughtsOrCrosses) {
 
         int resultU1 = 0;
         int resultU2 = 0;
@@ -167,67 +144,27 @@ public class Main {
             int resultW = 0;
             int resultK = 0;
             for (int j = 0; j < SizeY; ++j) {
-                if (tablica[i][j] == 'x') {
+                if (tablica[i][j] == noughtsOrCrosses) {
                     ++resultW;
                     if (resultW == 3) {
                         return true;
                     }
 
                 }
-                if (tablica[j][i] == 'x') {
+                if (tablica[j][i] == noughtsOrCrosses) {
                     ++resultK;
                     if (resultK == 3) {
                         return true;
                     }
 
                 }
-                if (tablica[i][j] == 'x' && i == j) {
+                if (tablica[i][j] == noughtsOrCrosses && i == j) {
                     ++resultU1;
                     if (resultU1 == 3) {
                         return true;
                     }
                 }
-                if (tablica[i][j] == 'x' && i + j == 2) {
-                    ++resultU2;
-                    if (resultU2 == 3) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-
-    }
-
-    private static boolean toWInO(char[][] tablica, int SizeX, int SizeY) {
-
-        int resultU1 = 0;
-        int resultU2 = 0;
-        for (int i = 0; i < SizeX; ++i) {
-            int resultW = 0;
-            int resultK = 0;
-            for (int j = 0; j < SizeY; ++j) {
-                if (tablica[i][j] == 'o') {
-                    ++resultW;
-                    if (resultW == 3) {
-                        return true;
-                    }
-
-                }
-                if (tablica[j][i] == 'o') {
-                    ++resultK;
-                    if (resultK == 3) {
-                        return true;
-                    }
-
-                }
-                if (tablica[i][j] == 'o' && i == j) {
-                    ++resultU1;
-                    if (resultU1 == 3) {
-                        return true;
-                    }
-                }
-                if (tablica[i][j] == 'o' && i + j == 2) {
+                if (tablica[i][j] == noughtsOrCrosses && i + j == 2) {
                     ++resultU2;
                     if (resultU2 == 3) {
                         return true;
